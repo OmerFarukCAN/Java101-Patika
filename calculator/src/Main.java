efb5a58fb9c33b59fc4ecfdc7df4de1fde451d11
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-15 23:51:00 +0300

    Project-8

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double n1, n2;
+        int selectedNumber;
+        int count = 4;
         Scanner sc = new Scanner(System.in);
 
-        int count = 4;
         while (count > 0) {
             System.out.print("Please enter the first number: ");
-            double n1 = sc.nextDouble();
+            n1 = sc.nextDouble();
             System.out.print("Please enter the second number: ");
-            double n2 = sc.nextDouble();
+            n2 = sc.nextDouble();
 
             System.out.println("1-Addition\n2-Subtraction\n3-Divide\n4-Multiply");
             System.out.print("Please select the action you want to do: ");
-            int selectedNumber = sc.nextInt();
+            selectedNumber = sc.nextInt();
 
             switch (selectedNumber) {
                 case 1:
