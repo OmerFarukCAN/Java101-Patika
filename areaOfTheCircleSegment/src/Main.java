edf0c59ff6f4a8f187dd874918247932e32ec8e9
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-15 23:46:40 +0300

    Project-5

--- areaOfTheCircleSegment/src/Main.java
+++ areaOfTheCircleSegment/src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double radius, centerAngle, area;
+        double pi = Math.PI;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Radius of circle: ");
-        double radius = sc.nextDouble();
+        radius = sc.nextDouble();
         System.out.print("Center angle of circle: ");
-        double centerAngle = sc.nextDouble();
-        double pi = Math.PI;
-        System.out.println(pi);
+        centerAngle = sc.nextDouble();
 
-        double area = (pi * (radius * radius) * centerAngle) / 360;
+        area = (pi * (radius * radius) * centerAngle) / 360;
         System.out.println("Area of the circle segment: " + area);
     }
 }
