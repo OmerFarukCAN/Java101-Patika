44d624c9636c04b6622153a0a956a8b47c19a54e
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-15 23:40:27 +0300

    Project-3

--- calculateTriangleArea/src/Main.java
+++ calculateTriangleArea/src/Main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int side1, side2, side3, perimeterTriangle, u;
+        double area;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("First side of triangle: ");
-        int side1 = sc.nextInt();
+        side1 = sc.nextInt();
         System.out.print("Second side of triangle: ");
-        int side2 = sc.nextInt();
+        side2 = sc.nextInt();
         System.out.print("Third side of triangle: ");
-        int side3 = sc.nextInt();
+        side3 = sc.nextInt();
 
-        int perimeterTriangle = side1 + side2 + side3;
-        int u = perimeterTriangle / 2;
+        perimeterTriangle = side1 + side2 + side3;
+        u = perimeterTriangle / 2;
 
-        double area = Math.sqrt((u * (u - side1) * (u - side2) * (u - side3)));
+        area = Math.sqrt((u * (u - side1) * (u - side2) * (u - side3)));
         System.out.println("Area of triangle: " + area);
     }
 }
