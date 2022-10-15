b50c8f09090b6588e4c215bbc587e35d14d6e44a
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-15 19:58:19 +0300

    Project-6

---
+++ calculateBodyMassIndex/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter your height in meters: ");
+        double height = sc.nextDouble();
+        System.out.print("Please enter your weight in kg: ");
+        double weight = sc.nextDouble();
+
+        double bodyMassIndex = (weight / (height * height));
+        System.out.println("Your body mass index: " + bodyMassIndex);
+    }
+}
