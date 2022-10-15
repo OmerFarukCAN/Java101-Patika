7450d708699c0d3b51fa4fa80f36a0f3b4bd971e
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-15 23:49:08 +0300

    Project-7

--- greengrocer/src/Main.java
+++ greengrocer/src/Main.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         double pearPerKg = 2.14;
         double applePerKg = 3.67;
         double tomatoPerKg = 1.11;
         double bananaPerKg = 0.95;
         double auberginePerKg = 5;
+        double pearKg, appleKg, tomatoKg, bananaKg, aubergineKg, totalAmount;
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Pear(kg): ");
-        double pearKg = sc.nextDouble();
+        pearKg = sc.nextDouble();
         System.out.print("Apple(kg): ");
-        double appleKg = sc.nextDouble();
+        appleKg = sc.nextDouble();
         System.out.print("Tomato(kg): ");
-        double tomatoKg = sc.nextDouble();
+        tomatoKg = sc.nextDouble();
         System.out.print("Banana(kg): ");
-        double bananaKg = sc.nextDouble();
+        bananaKg = sc.nextDouble();
         System.out.print("Aubergine(kg): ");
-        double aubergineKg = sc.nextDouble();
+        aubergineKg = sc.nextDouble();
 
-        double totalAmount = (pearPerKg * pearKg) + (applePerKg * appleKg) + (tomatoPerKg * tomatoKg) +
+        totalAmount = (pearPerKg * pearKg) + (applePerKg * appleKg) + (tomatoPerKg * tomatoKg) +
                 (bananaPerKg * bananaKg) + (auberginePerKg * aubergineKg);
 
         System.out.println("Total Amount: " + totalAmount);
