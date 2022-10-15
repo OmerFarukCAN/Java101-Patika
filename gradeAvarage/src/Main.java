246e90c1cef6df1f4472ac5d4efc9fd26c2f4c7f
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-16 00:23:41 +0300

    Project-1

--- gradeAvarage/src/Main.java
+++ gradeAvarage/src/Main.java
@@ -2,34 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int mathGrade, physicsGrade, chemistryGrade, turkishGrade, historyGrade, musicGrade;
-        Scanner inp = new Scanner(System.in);
+        int mathGrade, physicsGrade, chemistryGrade, turkishGrade, historyGrade, musicGrade, total;
+        double gradeAverage;
+        boolean passCondition;
+        String passTheClass;
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Your math grade: ");
-        mathGrade = inp.nextInt();
-
+        mathGrade = sc.nextInt();
         System.out.print("Your physics grade: ");
-        physicsGrade = inp.nextInt();
-
+        physicsGrade = sc.nextInt();
         System.out.print("Your chemistry grade: ");
-        chemistryGrade = inp.nextInt();
-
+        chemistryGrade = sc.nextInt();
         System.out.print("Your turkish grade: ");
-        turkishGrade = inp.nextInt();
+        turkishGrade = sc.nextInt();
+        System.out.print("Your history grade: ");
+        historyGrade = sc.nextInt();
+        System.out.print("Your music grade: ");
+        musicGrade = sc.nextInt();
 
-        System.out.print("Your history grade : ");
-        historyGrade = inp.nextInt();
-
-        System.out.print("Your music grade : ");
-        musicGrade = inp.nextInt();
-
-        int total = mathGrade + physicsGrade + chemistryGrade + turkishGrade + historyGrade + musicGrade;
-        double gradeAverage = total / 6.0;
+        total = mathGrade + physicsGrade + chemistryGrade + turkishGrade + historyGrade + musicGrade;
+        gradeAverage = total / 6.0;
         System.out.println("Your Grade Average: " + gradeAverage);
 
-        boolean passCondition = gradeAverage >= 50;
-        String passTheClass = passCondition ? "You passed the class." : "You failed the class.";
+        passCondition = gradeAverage >= 50;
+        passTheClass = passCondition ? "You passed the class." : "You failed the class.";
         System.out.println(passTheClass);
-
     }
 }
