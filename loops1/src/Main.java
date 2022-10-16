48bdae29f39094ea873d0c6803d27d0be18a8a16
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-16 23:59:24 +0300

    Project-16

--- loops1/src/Main.java
+++ loops1/src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int number;
-        int total = 0;
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Please enter number: ");
         number = sc.nextInt();
 
         for (int i = 0; i < number; i++) {
-            if (i % 3 == 0 && i % 4 == 0) total += i;
+            if (i % 3 == 0 && i % 4 == 0) sum += i;
         }
-        System.out.println("Total: " + total);
+        System.out.println("The sum of numbers divisible by 3 and 4: " + sum);
     }
 }
