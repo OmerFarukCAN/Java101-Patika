30975d84b5f3ae374f00ec1fac48885d8695e71b
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-24 19:38:42 +0300

    Project-32

--- recursivePower/src/Main.java
+++ recursivePower/src/Main.java
@@ -4,23 +4,24 @@ public class Main {
     static int result = 1;
 
     static int power(int n1, int n2) {
-        if (n1 == 0) {
+        if (n2 == 0) {
             return 1;
         }
-        result *= n2;
-        power(n1 - 1, n2);
+        result *= n1;
+        power(n1, n2 - 1);
         return result;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int baseNumber, exponentialNumber;
+        int baseNumber, exponentialNumber, result;
 
-        System.out.println("Please enter base number: ");
+        System.out.print("Please enter base number: ");
         baseNumber = input.nextInt();
-        System.out.println("Please enter the exponential number: ");
+        System.out.print("Please enter the exponential number: ");
         exponentialNumber = input.nextInt();
 
-        System.out.println(power(baseNumber, exponentialNumber));
+        result = power(baseNumber, exponentialNumber);
+        System.out.println(result);
     }
 }
